2bc13801cbfadb7585e508a369377978cd915fee
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-22 23:13:52 -0500

    Complete kwaj expansion

--- src/kwaj_unpack.cpp
+++ src/kwaj_unpack.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
 #include <string>
 #include "ArgParseStandalone.h"
+#include <unistd.h>
+#include "mspack.h"
 
-int main(int argc [[maybe_unused]], char** argv [[maybe_unused]]) {
+int main(int argc, char** argv) {
+    // Argument handling
     std::string input_filepath = "";
-    ArgParse::ArgParser Parse("kwaj_unpack");
-    Parse.AddArgument("-i", "Input file", &input_filepath);
-    if(Parse.ParseArgs(argc, argv) < 0) {
+    std::string output_filepath = "";
+    ArgParse::ArgParser Parser("kwaj_unpack");
+    Parser.AddArgument("-i", "Input file", &input_filepath);
+    Parser.AddArgument("-o", "Output file", &output_filepath);
+    if(Parser.ParseArgs(argc, argv) < 0) {
         std::cerr << "Problem parsing arguments" << std::endl;
         return 1;
     }
-    std::cout << "Input file: " << input_filepath << std::endl;
+    if(Parser.HelpPrinted()) {
+        return 0;
+    }
+
+    // Check if input file exists
+    if(access(input_filepath.c_str(), F_OK) == -1) {
+        std::cerr << "Input file doesn't exist!" << std::endl;
+        return 1;
+    }
+
+    // Check that we can use libmspack
+    int selftest_result = 0;
+    MSPACK_SYS_SELFTEST(selftest_result);
+    if(selftest_result != MSPACK_ERR_OK) {
+        std::cerr << "Incompatible with this build of libmspack\n" << std::endl;
+        return 1;
+    }
+    
+    // Create a decompressor
+    struct mskwaj_decompressor* decompressor = mspack_create_kwaj_decompressor(NULL);
+    if(decompressor == NULL) {
+        std::cerr << "There was a problem creating the decompressor" << std::endl;
+        return 1;
+    }
+
+    // Decompress
+    int result = decompressor->decompress(decompressor, input_filepath.c_str(), output_filepath.c_str());
+    if(result != MSPACK_ERR_OK) {
+        std::cerr << "There was an error decompressing!" << std::endl;
+        mspack_destroy_kwaj_decompressor(decompressor);
+        return 1;
+    }
+
+    // clean up the decompressor
+    mspack_destroy_kwaj_decompressor(decompressor);
     return 0;
 }
