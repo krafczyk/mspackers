fb175a8c03e45b440a9b810b3eb4650b2f6f29cc
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-22 21:48:55 -0500

    Basic argument parsing

--- src/kwaj_unpack.cpp
+++ src/kwaj_unpack.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <string>
+#include "ArgParseStandalone.h"
+
 int main(int argc [[maybe_unused]], char** argv [[maybe_unused]]) {
+    std::string input_filepath = "";
+    ArgParse::ArgParser Parse("kwaj_unpack");
+    Parse.AddArgument("-i", "Input file", &input_filepath);
+    if(Parse.ParseArgs(argc, argv) < 0) {
+        std::cerr << "Problem parsing arguments" << std::endl;
+        return 1;
+    }
+    std::cout << "Input file: " << input_filepath << std::endl;
     return 0;
 }
